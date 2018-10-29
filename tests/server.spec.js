67f590aa2d4e94f6ad4d3715ee7425da4e84f495
michaelagard <mdagard@gmail.com>
2018-10-29 14:31:35 -0400

    GET /api/notes tests

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -2,8 +2,20 @@ const request = require('supertest');
 const server = require('../api/server.js');
 
 describe('../api/server.js', () => {
+
   describe('GET /api/notes', () => {
 
+    it('should return status code 200 if notes exist', async () => {
+      const response = await request(server).get(`/api/notes`);
+      const expected = 200;
+      expect(response.status).toBe(expected);
+    });
+
+    it('should return status code 204 if notes do not exist', async () => {
+      const response = await request(server).get(`/api/notes`);
+      const expected = 204;
+      expect(response.status).toBe(expected);
+    });
   });
   describe('GET /api/notes/:id', () => {
 
