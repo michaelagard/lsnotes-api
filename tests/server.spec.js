e12782b8095eb08f11cc3f9af8fbb650fa690633
michaelagard <mdagard@gmail.com>
2018-10-29 17:09:53 -0400

    add more tests for get and post

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -11,21 +11,69 @@ describe('../api/server.js', () => {
       expect(response.status).toBe(expected);
     });
 
-    it('should return status code 204 if notes do not exist', async () => {
-      const response = await request(server).get(`/api/notes`);
-      const expected = 204;
+    it.skip('should return status code 500 if notes do not exist', async () => {
+      const response = await request(server).get(`/api/notes/1`);
+      const expected = 500;
       expect(response.status).toBe(expected);
     });
   });
+
   describe('GET /api/notes/:id', () => {
 
-  });
-  describe('POST /api/notes/create', () => {
+    it('should return status code 200 if notes exist', async () => {
+      const response = await request(server).get(`/api/notes`);
+      const expected = 200;
+      expect(response.status).toBe(expected);
+    });
 
+    it('should return status code 404 if note does not exist', async () => {
+      const response = await request(server).get(`/api/notes/:thiswontexist`);
+      const expected = 404;
+      expect(response.status).toBe(expected);
+    });
   });
+
+  describe('POST /api/notes/create/:bodyData', () => {
+
+    it('should return status code 200 if notes exist', async () => {
+      let bodyData = { title: 'JEST POST TEST', tags: 'JEST, POST', textBody: 'THIS IS A TEST FROM JEST' }
+      const response = await request(server).post(`/api/notes/create`).send(bodyData);
+      const expected = 201;
+      expect(response.status).toBe(expected);
+    });
+
+    it('should return status code 422 if body does not exist', async () => {
+      const response = await request(server).post(`/api/notes/create`).send();
+      const expected = 422;
+      expect(response.status).toBe(expected);
+    });
+
+    it('should return status code 422 if note.title does not exist', async () => {
+      let bodyData = { tags: 'JEST, POST', textBody: 'THIS IS A TEST FROM JEST' }
+      const response = await request(server).post(`/api/notes/create`).send(bodyData);
+      const expected = 422;
+      expect(response.status).toBe(expected);
+    });
+
+    it('should return status code 422 if note.body does not exist', async () => {
+      let bodyData = { tags: 'JEST, POST', textBody: 'THIS IS A TEST FROM JEST' }
+      const response = await request(server).post(`/api/notes/create`).send(bodyData);
+      const expected = 422;
+      expect(response.status).toBe(expected);
+    });
+
+    it.skip('should respond with the id of the new note', async () => {
+      let bodyData = { title: 'JEST POST TEST', tags: 'JEST, POST', textBody: 'THIS IS A TEST FROM JEST' }
+      const response = await request(server).post(`/api/notes/create`).send(bodyData);
+      const expected = 6;
+      expect(response.body).toBe(expected);
+    });
+  });
+
   describe('PUT /api/notes/edit/:id', () => {
 
   });
+
   describe('DELETE /api/notes/delete/:id', () => {
 
   });
