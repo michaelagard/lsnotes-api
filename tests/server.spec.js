00f819d720276b5e32a8b1314415d3d7456f123b
michaelagard <mdagard@gmail.com>
2018-10-29 17:43:47 -0400

    add DELETE /api/notes/delete/:id testing

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -99,5 +99,36 @@ describe('../api/server.js', () => {
 
   describe('DELETE /api/notes/delete/:id', () => {
 
+    it.skip('should return status code 200 if existing note was deleted correctly', async () => {
+      let id = 4;
+      const response = await request(server).delete(`/api/notes/delete/${id}`);
+      const expected = 200;
+      expect(response.status).toBe(expected);
+    });
+
+    it('should return status code 404 if deleted note does note exist', async () => {
+      let id = 4;
+      const response = await request(server).delete(`/api/notes/delete/${id}`);
+      const expected = 404;
+      expect(response.status).toBe(expected);
+    });
+
+    it('should return with count of deleted records which is 1', async () => {
+      let bodyData = { id: 5000, title: 'JEST POST TEST', tags: 'JEST, POST', textBody: 'THIS IS A TEST FROM JEST' }
+      const postResponse = await request(server).post(`/api/notes/create`).send(bodyData);
+      const postExpected = 201;
+      expect(postResponse.status).toBe(postExpected);
+      let id = 5000;
+      const response = await request(server).delete(`/api/notes/delete/${id}`);
+      const expected = 1;
+      expect(response.body).toBe(expected);
+    })
+
+    it('should return with "message: No records found to delete"', async () => {
+      let id = 'nothingHere';
+      const response = await request(server).delete(`/api/notes/delete/${id}`);
+      const expected = { message: 'No records found to delete' };
+      expect(response.body).toEqual(expected);
+    })
   });
 });
