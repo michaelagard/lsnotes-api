924ab302e2b48c2856ed6e1399aad0f47f659a1b
michaelagard <mdagard@gmail.com>
2018-10-29 15:29:12 -0400

    hardcoded port and name in listen

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-const server = require('./api/server.js'); // import api endpoints
-
-server.listen(serverPort, () => console.log(`## ${serverName} running on port ${serverPort} ##`));
+const server = require('./api/server'); // import api endpoints
+server.listen(process.env.PORT || 7100, () => console.log(`## lsnotes - api running on port ${process.env.PORT || 7100} ##`));
