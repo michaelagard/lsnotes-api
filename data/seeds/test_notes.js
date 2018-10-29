b22f7c35c28fc1ee762582c88b9101b7b9ea3bd2
michaelagard <mdagard@gmail.com>
2018-10-29 17:08:55 -0400

    renamed noteId to id to seeded data

--- data/seeds/test_notes.js
+++ data/seeds/test_notes.js
@@ -4,9 +4,9 @@ exports.seed = function (knex, Promise) {
   return knex('notes').del()
     .then(function () {
       return knex('notes').insert([
-        { noteId: 1, title: 'First Seeded Note', tags: 'first', textBody: 'This is the first seeded note.' },
-        { noteId: 2, title: 'Second Seeded Note', tags: 'second', textBody: 'This is the second seeded note.' },
-        { noteId: 3, title: 'Third Seeded Note', tags: 'third', textBody: 'This is the third seeded note.' },
+        { id: 1, title: 'First Seeded Note', tags: 'first', textBody: 'This is the first seeded note.' },
+        { id: 2, title: 'Second Seeded Note', tags: 'second', textBody: 'This is the second seeded note.' },
+        { id: 3, title: 'Third Seeded Note', tags: 'third', textBody: 'This is the third seeded note.' },
       ]);
     });
 };
