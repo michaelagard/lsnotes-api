973a6457173d498ce81a7de59076699dbbd7727f
michaelagard <mdagard@gmail.com>
2018-10-29 14:17:10 -0400

    add testing boilerplate code

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,4 +1,20 @@
 const request = require('supertest');
 const server = require('../api/server.js');
 
-describe('../api/server.js', () => { });
+describe('../api/server.js', () => {
+  describe('GET /api/notes', () => {
+
+  });
+  describe('GET /api/notes/:id', () => {
+
+  });
+  describe('POST /api/notes/create', () => {
+
+  });
+  describe('PUT /api/notes/edit/:id', () => {
+
+  });
+  describe('DELETE /api/notes/delete/:id', () => {
+
+  });
+});
