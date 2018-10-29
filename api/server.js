488ad5cadad91899b58fe4d4b1c1aadae364b957
michaelagard <mdagard@gmail.com>
2018-10-29 14:17:00 -0400

    add knex settings

--- api/server.js
+++ api/server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+// CONFIG: knex settings
+
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
 // CONFIG: server settings
 const serverPort = process.env.PORT || 7100; // server port
 const serverName = `lsnotes-api`; // Name of server to display at "/" endpoint 
@@ -10,7 +17,7 @@ const projectPullRequest = `https://github.com/michaelagard/lsnotes-api/pull/2`;
 
 // ENDPOINTS
 server.get('/', (req, res) => { // sanity check root endpoint
-  res.send(`${serverName} running on port ${serverPort}<br>More information: <a href="${projectPullRequest}">n/a</a>`);
+  res.status(200).send(`${serverName} running on port ${serverPort}<br>More information: <a href="${projectPullRequest}">n/a</a>`);
 });
 
 module.exports = server;
