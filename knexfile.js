5e5675dafd44ab863cd875cf89f47ab494d6ac7c
michaelagard <mdagard@gmail.com>
2018-10-29 16:13:28 -0400

    moved to root since bug was occuring when it was in the data folder

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/notes.sqlite3'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    useNullAsDefault: true,
+  }
+};
