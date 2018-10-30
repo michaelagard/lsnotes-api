8337cf5c94fb695f5a5473d2c800bbd0afa670ae
michaelagard <mdagard@gmail.com>
2018-10-30 16:44:12 -0400

    remove __v and add tags to seed file

--- data/seeds/test_notes.js
+++ data/seeds/test_notes.js
@@ -4,9 +4,9 @@ exports.seed = function (knex, Promise) {
   return knex('notes').del()
     .then(function () {
       return knex('notes').insert([
-        { _id: 1, title: 'First Seeded Note', textBody: 'This is the first seeded note.', __v: 0 },
-        { _id: 2, title: 'Second Seeded Note', textBody: 'This is the second seeded note.', __v: 0 },
-        { _id: 3, title: 'Third Seeded Note', textBody: 'This is the third seeded note.', __v: 0 },
+        { _id: 1, tags: 'tag1, tag2', title: 'First Seeded Note', textBody: 'This is the first seeded note.' },
+        { _id: 2, tags: 'tag3, tag4', title: 'Second Seeded Note', textBody: 'This is the second seeded note.' },
+        { _id: 3, tags: 'tag5, tag6', title: 'Third Seeded Note', textBody: 'This is the third seeded note.' },
       ]);
     });
 };
