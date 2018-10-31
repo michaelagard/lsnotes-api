9d4b7e7414c0da7fcc905058b7a062e444b7b4f1
michaelagard <mdagard@gmail.com>
2018-10-31 16:48:32 -0400

    add production configuration to use pg

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,13 @@
+require('donenv').config();
+const logalPg = {
+  host: 'localhost',
+  database: 'lambda',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
+}
+
+const dbConnection = process.env.DATABASE_URL || localPg;
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -11,5 +21,20 @@ module.exports = {
       directory: './data/migrations'
     },
     useNullAsDefault: true,
+  },
+  production: {
+    client: 'pg',
+    connetion: dbConneciton,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    }
   }
 };
