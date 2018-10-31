0e9f5140756333aa841085bfc2c5cfa4778d8512
michaelagard <mdagard@gmail.com>
2018-10-31 16:49:26 -0400

    restructure port and require dotenv

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+require('dotenv').config();
 const server = require('./api/server'); // import api endpoints
-server.listen(process.env.PORT || 7100, () => console.log(`## lsnotes - api running on port ${process.env.PORT || 7100} ##`));
+const port = process.env.PORT || 7100
+server.listen(port, () => console.log(`## lsnotes - api running on port ${port} ##`));
